8505b757a91cd8902676b7b7cb196d94e7917c41
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-02-22 21:32:34 -0300

    Category-System

---
+++ New-Category-System/resources/views/pages/registros/regVendas.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.regBase')
+@section('style')
+    <link rel="stylesheet" href="regPages/regVendas/style.css">
+@endsection
+@section('title', 'Registro de Vendas')
+@section('content')
+
+@endsection
