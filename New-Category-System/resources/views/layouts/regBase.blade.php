3f9d75de549ac15a5458021d4e7eb9876f876011
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-03-05 21:18:06 -0300

    New-Category-System

--- New-Category-System/resources/views/layouts/regBase.blade.php
+++ New-Category-System/resources/views/layouts/regBase.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Gillian Melo Franco">
     <link rel="icon" type="image/x-icon" href="default/imgs/database-favicon.png">
     <link rel="stylesheet" href="default/regBase/style.css">
     @yield('style')
